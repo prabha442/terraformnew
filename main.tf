5a547c7bbbc95e3dc07be3c890fee3a7a2e2bd68
prabhakar <gprabha013@gmail.com>
2022-03-29 18:12:18 +0530

    adding s3-bucket files

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws"  {
+    region = var.aws-region
+  
+}
+
+resource "aws_s3_bucket" "my-s3-bucket" {
+    bucket_prefix = var.bucket_prefix
+    acl = var.acl
+
+    versioning {
+        enabled = var.versioning
+    }
+tags = var.tags
+
+}
---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "aws-region" {
+    default = "us-east-1"
+    dedescription = "this is the default us-est-1 region"  
+}
+
+variable "bucket_prefix" {
+    type = string
+    default = "s3-bucket-"
+    dedescription = "by using bucket prifix it can create its own default name "
+   
+}
+
+variable "acl" {
+    type = string
+    default = "private"
+    description = "default it is private"
+  
+}
+
+variable "versioning" {
+    type = bool
+    default = true
+    description = "(optional) a state of versioning."
+  
+}
+ variable "tags" {
+     type = map
+     description = "(optional) a maping tags to assign to bucket"
+     default = {
+         environment = "DEV"
+         terraform = "true"
+     }
+
+   
+ }
